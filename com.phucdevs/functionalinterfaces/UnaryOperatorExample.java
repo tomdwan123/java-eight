a74f6fad1b5d647a42a5280e535c4acbefbc6b85
phuc.letan <phuc.letan@messagemedia.com>
2023-04-16 11:56:24 +0700

    ✨ Init unaryOperator

---
+++ com.phucdevs/functionalinterfaces/UnaryOperatorExample.java
@@ -0,0 +1,13 @@
+package functionalinterfaces;
+
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorExample {
+
+    static UnaryOperator<String> unaryOperator = s -> s.concat("default");
+
+    public static void main(String[] args) {
+
+        System.out.println(unaryOperator.apply("java8"));
+    }
+}
