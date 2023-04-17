5c933e6bd7237dd1cc7aa552873c952b2a8598a7
phuc.letan <phuc.letan@messagemedia.com>
2023-04-17 21:43:07 +0700

    ✨ Init streams min max

---
+++ com.phucdevs/streams/StreamsMinMaxExample.java
@@ -0,0 +1,31 @@
+package streams;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class StreamsMinMaxExample {
+
+    public static long findMaxValue() {
+
+        return IntStream.rangeClosed(6, 10)
+                .asLongStream()
+                .reduce(0, Long::max);
+    }
+
+    public static Optional<Long> findMaxValueWithOptional() {
+
+        return Optional.of(IntStream.rangeClosed(6, 10)
+                .asLongStream()
+                .reduce(0, Long::max));
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("findMaxValue: " + findMaxValue());
+
+        Optional<Long> maxValueWithOptional = findMaxValueWithOptional();
+        if (maxValueWithOptional.isPresent()) {
+            System.out.println("findMaxValueWithOptional: " + maxValueWithOptional.get());
+        }
+    }
+}
