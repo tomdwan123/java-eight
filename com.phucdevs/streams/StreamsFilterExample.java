975ac524fccdff035937345b623f73394d127f2c
phuc.letan <phuc.letan@messagemedia.com>
2023-04-17 21:09:20 +0700

    ✨ Init streams filter

---
+++ com.phucdevs/streams/StreamsFilterExample.java
@@ -0,0 +1,25 @@
+package streams;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsFilterExample {
+
+    public static List<Student> filterStudents() {
+
+        return StudentDataBase.getAllStudents().stream() // Stream<Student>
+                .filter(student -> student.getGender().equals("female")) // Stream<Student>
+                // filters and sends only the students whose gender is female
+                .filter(student -> student.getGpa() >= 3.9)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("filterStudents: ");
+        filterStudents().forEach(System.out::println);
+    }
+}
