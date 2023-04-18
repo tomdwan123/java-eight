d89c01040eac8998169cc4ee285449822d584728
phuc.letan <phuc.letan@messagemedia.com>
2023-04-18 17:24:39 +0700

    ✨Init streams groupingBy()

--- com.phucdevs/streams_terminal/StreamsGroupingByExample.java
+++ com.phucdevs/streams_terminal/StreamsGroupingByExample.java
@@ -3,10 +3,14 @@ package streams_terminal;
 import data.Student;
 import data.StudentDataBase;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.toSet;
 
 public class StreamsGroupingByExample {
 
@@ -24,9 +28,43 @@ public class StreamsGroupingByExample {
                 .collect(groupingBy(student -> student.getGpa() >= 3.8 ? "OUTSTANDING" : "AVERAGE"));
     }
 
+    public static Map<Integer, Map<String, List<Student>>> twoLevelGrouping1() {
+
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .collect(
+                        groupingBy(
+                                Student::getGradeLevel,
+                                groupingBy(student -> student.getGpa() >= 3.8 ? "OUTSTANDING" : "AVERAGE")
+                        )
+                );
+    }
+
+    public static Map<String, Integer> twoLevelGrouping2() {
+
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .collect(
+                        groupingBy(
+                                Student::getName,
+                                summingInt(Student::getNoteBooks)
+                        )
+                );
+    }
+
+    public static LinkedHashMap<String, Set<Student>> threeArgumentsGroupBy() {
+
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getName, LinkedHashMap::new, toSet()));
+    }
+
     public static void main(String[] args) {
 
         System.out.println("groupStudentGroupingByGender: " + groupStudentGroupingByGender());
         System.out.println("groupStudentGrouping: " + groupStudentGrouping());
+        System.out.println("twoLevelGrouping1: " +twoLevelGrouping1());
+        System.out.println("twoLevelGrouping2: " + twoLevelGrouping2());
+        System.out.println("threeArgumentsGroupBy: " + threeArgumentsGroupBy());
     }
 }
