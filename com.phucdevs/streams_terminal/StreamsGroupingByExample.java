a023ab31b3e1d6ccde0ff939c55749bea7175799
phuc.letan <phuc.letan@messagemedia.com>
2023-04-18 17:34:06 +0700

    ✨Init streams groupingBy()

--- com.phucdevs/streams_terminal/StreamsGroupingByExample.java
+++ com.phucdevs/streams_terminal/StreamsGroupingByExample.java
@@ -3,12 +3,16 @@ package streams_terminal;
 import data.Student;
 import data.StudentDataBase;
 
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
+import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.maxBy;
 import static java.util.stream.Collectors.summingInt;
 import static java.util.stream.Collectors.toSet;
 
@@ -59,6 +63,29 @@ public class StreamsGroupingByExample {
                 .collect(groupingBy(Student::getName, LinkedHashMap::new, toSet()));
     }
 
+    public static void calculateTopGpa() {
+
+        Map<Integer, Optional<Student>> studentMapOptional = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(
+                        groupingBy(
+                                Student::getGradeLevel,
+                                maxBy(Comparator.comparing(Student::getGpa))
+                        )
+                );
+        System.out.println(studentMapOptional);
+
+        Map<Integer, Object> studentMapOptional1 = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(
+                        groupingBy(
+                                Student::getGradeLevel,
+                                collectingAndThen(maxBy(Comparator.comparing(Student::getGpa)),
+                                Optional::get))
+                );
+        System.out.println(studentMapOptional1);
+    }
+
     public static void main(String[] args) {
 
         System.out.println("groupStudentGroupingByGender: " + groupStudentGroupingByGender());
@@ -66,5 +93,6 @@ public class StreamsGroupingByExample {
         System.out.println("twoLevelGrouping1: " +twoLevelGrouping1());
         System.out.println("twoLevelGrouping2: " + twoLevelGrouping2());
         System.out.println("threeArgumentsGroupBy: " + threeArgumentsGroupBy());
+        calculateTopGpa();
     }
 }
