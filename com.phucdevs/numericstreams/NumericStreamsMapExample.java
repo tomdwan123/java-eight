affe143a998d63f9239fcb1919369d2bc52e8f63
phuc.letan <phuc.letan@messagemedia.com>
2023-04-18 16:30:37 +0700

    ✨ Init numeric streams map example

---
+++ com.phucdevs/numericstreams/NumericStreamsMapExample.java
@@ -0,0 +1,38 @@
+package numericstreams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NumericStreamsMapExample {
+
+    public static List<Integer> mapToObj() {
+
+        return IntStream.rangeClosed(1, 5)
+                .mapToObj(i -> {
+                    return new Integer(i);
+                })
+                .collect(Collectors.toList());
+    }
+
+    public static long mapToLong() {
+
+        return IntStream.rangeClosed(1, 5)
+                .mapToLong(i -> i)
+                .sum();
+    }
+
+    public static double mapToDouble() {
+
+        return IntStream.rangeClosed(1, 5)
+                .mapToDouble(i -> i)
+                .sum();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("mapToObj: " + mapToObj());
+        System.out.println("mapToLong: " + mapToLong());
+        System.out.println("mapToDouble: " + mapToDouble());
+    }
+}
