cb6c6702c50190d9545b3a5ffd5ef1b67f10bfcf
phuc.letan <phuc.letan@messagemedia.com>
2023-04-21 00:52:37 +0700

    ✨Optional: isPresent(), ifPresent()

---
+++ com.phucdevs/optional/OptionalPresentExample.java
@@ -0,0 +1,19 @@
+package optional;
+
+import java.util.Optional;
+
+public class OptionalPresentExample {
+
+    public static void main(String[] args) {
+
+        // isPresent
+        Optional<String> optional = Optional.ofNullable("Hello Optional");
+        System.out.println(optional.isPresent());
+        if (optional.isPresent()) {
+            System.out.println(optional.get());
+        }
+
+        // ifPresent
+        optional.ifPresent(s -> System.out.println(s));
+    }
+}
