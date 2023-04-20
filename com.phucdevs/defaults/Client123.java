62de371cbf447f89bfb9f2b2d75cd6165d8d7656
phuc.letan <phuc.letan@messagemedia.com>
2023-04-21 02:06:26 +0700

    ✨Interfaces

--- com.phucdevs/defaults/Client123.java
+++ com.phucdevs/defaults/Client123.java
@@ -7,6 +7,9 @@ public class Client123 implements Interface1, Interface2, Interface3 {
         System.out.println("Inside Method A: " + Client123.class);
     }
 
+    // 1 -> class the implements the interface
+    // 2 -> the sub interface that extends the interface
+
     public static void main(String[] args) {
 
         Client123 client123 = new Client123();
