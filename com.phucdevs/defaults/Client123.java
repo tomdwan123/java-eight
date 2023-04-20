15837c97523f848dc1a2e77d38fcd5c0899e21df
phuc.letan <phuc.letan@messagemedia.com>
2023-04-21 02:05:31 +0700

    ✨Interfaces

---
+++ com.phucdevs/defaults/Client123.java
@@ -0,0 +1,17 @@
+package defaults;
+
+public class Client123 implements Interface1, Interface2, Interface3 {
+
+    public void methodA() {
+
+        System.out.println("Inside Method A: " + Client123.class);
+    }
+
+    public static void main(String[] args) {
+
+        Client123 client123 = new Client123();
+        client123.methodA(); // remove to child implementation
+        client123.methodB();
+        client123.methodC();
+    }
+}
